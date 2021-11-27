4b4927d68d6fece9ba6c8cf0c30354f22fae1485
RuslanPr0g <theruslan.prog@gmail.com>
2021-11-27 14:43:33 +0200

    meth

--- RefrlectionPrikoli/Program.cs
+++ RefrlectionPrikoli/Program.cs
@@ -24,9 +24,20 @@ foreach (var type in types)
     var props = type.GetProperties();
     foreach (var prop in props)
     {
-        Console.WriteLine("Propname: " + prop.Name);
-        Console.WriteLine("Proptype: " + prop.PropertyType);
-        Console.WriteLine("Propmodule: " + prop.Module);
+        Console.WriteLine("\tPropname: " + prop.Name);
+        Console.WriteLine("\tProptype: " + prop.PropertyType);
+        Console.WriteLine("\tPropmodule: " + prop.Module);
+    }
+
+    var meths = type.GetMethods();
+    foreach (var meth in meths)
+    {
+        Console.WriteLine("\t\tMethname: " + meth.Name);
+        Console.WriteLine("\t\tMethtype: " + meth.ReturnType);
+        Console.WriteLine("\t\tMethmodule: " + meth.Module);
     }
     Console.WriteLine();
 }
+
+// ----------------------------------------------------------------------------
+
