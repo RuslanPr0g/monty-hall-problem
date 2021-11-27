8dfe30b8c521bbef68a890560f178492183f7f14
RuslanPr0g <theruslan.prog@gmail.com>
2021-11-27 14:35:23 +0200

    Added some examples

---
+++ RefrlectionPrikoli/Kek.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefrlectionPrikoli
+{
+    public class Kek
+    {
+        public int MyProperty { get; set; }
+        public string? Lole { get; set; }
+        public Lol LOL { get; set; }
+    }
+
+    public struct Lol
+    {
+        public int MyProperty { get; set; }
+        public Kek KEK { get; set; }
+        public bool IsMeme { get; set; }
+    }
+}
--- RefrlectionPrikoli/Program.cs
+++ RefrlectionPrikoli/Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿using System.Reflection;
+
+var ass = Assembly.GetExecutingAssembly();
+
+Console.WriteLine(ass.FullName);
+
+Console.WriteLine(ass.ExportedTypes);
+
+Console.WriteLine(ass.DefinedTypes);
+
+Console.WriteLine(ass.CodeBase);
+
+Console.WriteLine(ass.HostContext);
+
+var types = ass.GetTypes();
+
+foreach (var type in types)
+{
+    Console.WriteLine("Name: " + type.Name);
+    Console.WriteLine("FullName: " + type.FullName);
+    Console.WriteLine("BaseType: " + type.BaseType);
+    Console.WriteLine("CustomAttrs: " + type.CustomAttributes);
+
+    var props = type.GetProperties();
+    foreach (var prop in props)
+    {
+        Console.WriteLine("Propname: " + prop.Name);
+        Console.WriteLine("Proptype: " + prop.PropertyType);
+        Console.WriteLine("Propmodule: " + prop.Module);
+    }
+    Console.WriteLine();
+}
